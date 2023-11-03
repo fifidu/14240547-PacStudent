53e031415565404b0b949c196331d263ab81cfea
fifidu <fifi.du@student.uts.edu.au>
2023-11-03 12:51:20 +1100

    Background music
    Removed intro sounds from Level 1 and added to loop on Start Scene

--- Assets/Scripts/StartScreenMusic.cs
+++ Assets/Scripts/StartScreenMusic.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundMusic : MonoBehaviour
+public class StartScreenMusic : MonoBehaviour
 {
-    public AudioSource normalLoop;
+    public AudioSource introLoop;
 
     // Start is called before the first frame update
     void Start()
     {
-        normalLoop.Play();
+        introLoop.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
