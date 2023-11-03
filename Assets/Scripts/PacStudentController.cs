f7f08b2b9ad0c20adddebd96773f260069f4ba49
fifidu <fifi.du@student.uts.edu.au>
2023-11-03 21:16:10 +1100

    Primitive lerping
    Implemented lerp without grid

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -6,7 +6,10 @@ public class PacStudentController : MonoBehaviour
 {
     private Tweener tweener;
     KeyCode lastInput;
+    public GameObject pacStu;
     private bool isLerping;
+    private Vector3 startPos, endPos;
+    private float duration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +20,29 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("w"))
+        {
+            startPos = pacStu.transform.position;
+            endPos = new Vector3(startPos.x, startPos.y + 1.0f, startPos.z);
+            tweener.AddTween(pacStu.transform, startPos, endPos, duration);
+        }
         if (Input.GetKeyDown("a"))
         {
-            tweener.AddTween(this.transform, this.transform.position, new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
+            startPos = pacStu.transform.position;
+            endPos = new Vector3(startPos.x - 1.0f, startPos.y, startPos.z);
+            tweener.AddTween(pacStu.transform, startPos, endPos, duration);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            startPos = pacStu.transform.position;
+            endPos = new Vector3(startPos.x, startPos.y - 1.0f, startPos.z);
+            tweener.AddTween(pacStu.transform, startPos, endPos, duration);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            startPos = pacStu.transform.position;
+            endPos = new Vector3(startPos.x + 1.0f, startPos.y, startPos.z);
+            tweener.AddTween(pacStu.transform, startPos, endPos, duration);
         }
     }
 }
