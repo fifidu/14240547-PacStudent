13579b3bf2cb5649a982123fec2a9a9558bd5cfe
fifidu <fifi.du@student.uts.edu.au>
2023-11-03 22:21:27 +1100

    Added change of direction

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -7,14 +7,17 @@ public class PacStudentController : MonoBehaviour
     private Tweener tweener;
     KeyCode lastInput;
     public GameObject pacStu;
+    public Animator animator;
     private bool isLerping;
     private Vector3 startPos, endPos;
+    private Vector3 direction;
     private float duration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         tweener = GetComponent<Tweener>();
+        animator = pacStu.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,24 +25,28 @@ public class PacStudentController : MonoBehaviour
     {
         if (Input.GetKeyDown("w"))
         {
+            animator.Play("PacStudentUp");
             startPos = pacStu.transform.position;
             endPos = new Vector3(startPos.x, startPos.y + 1.0f, startPos.z);
             tweener.AddTween(pacStu.transform, startPos, endPos, duration);
         }
         if (Input.GetKeyDown("a"))
         {
+            animator.Play("PacStudentLeft");
             startPos = pacStu.transform.position;
             endPos = new Vector3(startPos.x - 1.0f, startPos.y, startPos.z);
             tweener.AddTween(pacStu.transform, startPos, endPos, duration);
         }
         if (Input.GetKeyDown("s"))
         {
+            animator.Play("PacStudentDown");
             startPos = pacStu.transform.position;
             endPos = new Vector3(startPos.x, startPos.y - 1.0f, startPos.z);
             tweener.AddTween(pacStu.transform, startPos, endPos, duration);
         }
         if (Input.GetKeyDown("d"))
         {
+            animator.Play("PacStudentRight");
             startPos = pacStu.transform.position;
             endPos = new Vector3(startPos.x + 1.0f, startPos.y, startPos.z);
             tweener.AddTween(pacStu.transform, startPos, endPos, duration);
